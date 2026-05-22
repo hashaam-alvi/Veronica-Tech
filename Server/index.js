bbc7922a0ed34c4107f44e390833837021a8de3a
Hashaam Alvi <hashaam.imran.alvi@gmail.com>
2026-05-22 12:02:31 +0500

    Updated Cors

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://yourfrontend.vercel.app",
+      "https://veronica-tech.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
